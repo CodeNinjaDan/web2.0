f6c963624bf69f1df9bd341b8b04d594cd11d3f2
CodeNinjaDan <dnlmuiru@gmail.com>
2025-10-15 23:34:54 +0300

    tailwind + daisy ui frontend

--- to-do_list/main.py
+++ to-do_list/main.py
@@ -23,7 +23,44 @@ class ToDo(db.Model):
 with app.app_context():
     db.create_all()
 
+@app.route('/')
+def home():
+    return get_all_todos()
+
 @app.route('/all')
 def get_all_todos():
     todos = db.session.execute(db.select(ToDo)).scalars().all()
     return render_template("index.html", all_todos=todos)
+
+@app.route('/add-sample')
+def add_sample_todos():
+    """Add sample todos for testing"""
+    existing = db.session.execute(db.select(ToDo)).scalars().first()
+    if not existing:
+        sample_todos = [
+            ToDo(
+                title="Complete Python Project",
+                description="Finish the Flask todo application with a beautiful frontend",
+                day=date(2025, 10, 25)
+            ),
+            ToDo(
+                title="Learn Tailwind CSS",
+                description="Master utility-first CSS framework for modern web design",
+                day=date(2025, 10, 23)
+            ),
+            ToDo(
+                title="Deploy Application",
+                description="Deploy the todo app to a cloud platform",
+                day=date(2025, 10, 30)
+            )
+        ]
+        db.session.add_all(sample_todos)
+        db.session.commit()
+        return "Sample todos added! <a href='/all'>View all todos</a>"
+    return "Todos already exist! <a href='/all'>View all todos</a>"
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
